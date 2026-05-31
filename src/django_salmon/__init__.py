e1dfc99a4493516ca41e65d082422ae2ddc331aa
Tim Schilling <schillingt@better-simple.com>
2026-05-31 11:02:28 -0500

    Add trusted publishing with support for test.pypi.org

--- src/django_salmon/__init__.py
+++ src/django_salmon/__init__.py
@@ -1,5 +1,12 @@
 from __future__ import annotations
 
+import os
+
 from .registry import registry
 
 __all__ = ["registry"]
+# Support suffixing the version with a dev segment.
+# This allows for building dev distributions to test
+# the release process.
+# See .github/workflows/test_release.yml
+__version__ = VERSION = "0.1.0" + os.environ.get("DJSALMON_VERSION_DEV", "")
